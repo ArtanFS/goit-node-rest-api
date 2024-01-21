8ffa86719a8fba94b497efe3a04fa8450c2806db
ArtanFS <132677965+ArtanFS@users.noreply.github.com>
2024-01-21 15:26:54 +0200

    init

---
+++ services/contactsServices.js
@@ -0,0 +1,59 @@
+import { readFile, writeFile } from "fs/promises";
+import { join } from "path";
+import { v4 as uuidv4 } from "uuid";
+
+const contactsPath = join(process.cwd(), "db", "contacts.json");
+
+async function listContacts() {
+  try {
+    const readData = await readFile(contactsPath);
+    return await JSON.parse(readData);
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+async function getContactById(contactId) {
+  try {
+    const dataArr = await listContacts();
+    const contact = dataArr.find((contact) => contact.id === contactId);
+    return contact || null;
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    const dataArr = await listContacts();
+    const contactIdx = dataArr.findIndex((contact) => contact.id === contactId);
+    if (contactIdx >= 0) {
+      const contact = dataArr[contactIdx];
+      dataArr.splice(contactIdx, 1);
+      await writeFile(contactsPath, JSON.stringify(dataArr));
+      return contact;
+    }
+    return null;
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+async function addContact(name, email, phone) {
+  try {
+    const dataArr = await listContacts();
+    const contact = {
+      id: uuidv4(),
+      name,
+      email,
+      phone,
+    };
+    dataArr.push(contact);
+    await writeFile(contactsPath, JSON.stringify(dataArr));
+    return contact;
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+export default { listContacts, getContactById, removeContact, addContact };
